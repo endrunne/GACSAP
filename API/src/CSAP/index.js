3fc15a79b9a18058e0862a539eb0aea27de7d977
Vitor Oliveira <endrunne@pm.me>
2020-11-16 22:39:33 -0300

    Using promise all to resolve all data request

--- API/src/CSAP/index.js
+++ API/src/CSAP/index.js
@@ -1,24 +1,25 @@
     const geneticAlgorithmConstructor = require('../index.js')
     const fetch = require("node-fetch");
     
-    let json = {};
+    let json = [];
 
     var getGroupsPhenotype = async function() {
-        let url = 'http://localhost:3000/api/classrooms';
-        await fetch(url)
+        let url = 'http://localhost:3000/api/groups';        
+        return await fetch(url)
         .then(res => res.json())
-        .then((groupData) => {
-            console.log(`RRRRRR: ${JSON.stringify(groupData)}`)
-            // return groupData;
+        .then((groupData) => {            
+            return groupData;
         })
         .catch(err => {throw err});
     }
 
     var getClassroomsPhenotype = async function() {        
-        let url = 'http://localhost:3000/api/groups';
-        await fetch(url)
+        let url = 'http://localhost:3000/api/classrooms';        
+        return await fetch(url)
         .then(res => res.json())
-        .then((clasroomData) => {return clasroomData;})
+        .then((groupData) => {            
+            return groupData;
+        })
         .catch(err => {throw err});
     }
 
@@ -32,76 +33,101 @@
         return [ phenotypeA , phenotypeB ]
     }
 
-    var fitnessFunction = function(phenotype) {
+    var fitnessFunction = async function(phenotype) {
         var score = 0
         // use your phenotype data to figure out a fitness score
 
-        var groupPhenotype = 
-        [
-            {
-                students: 6,
-                specialStudents: 2,
-                code: "CC7P17",
-                name: "Ciência da computação",
-                category: "Exatas"
-            },
-            {
-                students: 7,
-                specialStudents: 2,
-                code: "CC8P17",
-                name: "Ciencia da computacao",
-                category: "Exatas"                
-            },
-            {
-                students: 10,
-                specialStudents: 5,
-                code: "CC5P18",
-                name: "Ciência da computação",
-                category: "Exatas"
-            }
-        ]               
+        // Mock for tests
+        // var groupPhenotype = 
+        // [
+        //     {
+        //         students: 6,
+        //         specialStudents: 2,
+        //         code: "CC7P17",
+        //         name: "Ciência da computação",
+        //         category: "Exatas"
+        //     },
+        //     {
+        //         students: 7,
+        //         specialStudents: 2,
+        //         code: "CC8P17",
+        //         name: "Ciencia da computacao",
+        //         category: "Exatas"                
+        //     },
+        //     {
+        //         students: 10,
+        //         specialStudents: 5,
+        //         code: "CC5P18",
+        //         name: "Ciência da computação",
+        //         category: "Exatas"
+        //     }
+        // ]               
 
-        phenotype = {
-            normalSpaces: 7,
-            accessibleSpaces: 2,
-            code: "F-24",
-            name: "Sala de aula",
-            assignedGroup: { type: String},
-            attributes: [
-                {
-                    attributeName: "",
-                    attributeDisplayName: "",
-                    attributeValue: ""
+        // phenotype = {
+        //     normalSpaces: 7,
+        //     accessibleSpaces: 2,
+        //     code: "F-24",
+        //     name: "Sala de aula",
+        //     assignedGroup: { type: String},
+        //     attributes: [
+        //         {
+        //             attributeName: "",
+        //             attributeDisplayName: "",
+        //             attributeValue: ""
+        //         }
+        //     ]
+        // };
+
+        let promise = Promise.all([getClassroomsPhenotype(), getGroupsPhenotype()]);
+
+        promise.then(([classroomData, groupData]) => {
+            let groupPhenotype = [];
+            groupPhenotype.push(groupData);
+            
+            phenotype = [];
+            phenotype.push(classroomData);
+
+            for (var i = 0; i < groupPhenotype.length; i++)
+            {
+                var students = groupPhenotype[i].students;
+                var specialStudents = groupPhenotype[i].specialStudents;
+
+                if (phenotype[i].normalSpaces == students && phenotype[i].accessibleSpaces == specialStudents)
+                {    
+                    score = 10;
+                    phenotype[i].assignedGroup = groupPhenotype[i].code;
+                    groupFuntion(phenotype[i]);
+                    continue;
                 }
-            ]
-        };
-
-        for (var i = 0; i < groupPhenotype.length; i++)
-        {
-            var students = groupPhenotype[i].students;
-            var specialStudents = groupPhenotype[i].specialStudents;
-
-            if (phenotype.normalSpaces == students && phenotype.accessibleSpaces == specialStudents)
-            {    
-                score = 10;
-                phenotype.assignedGroup = groupPhenotype[i].code;
-                groupFuntion(phenotype);
-                break;
+                else if (phenotype[i].normalSpaces == students && phenotype[i].accessibleSpaces <= specialStudents)
+                {   
+                    score = 8;
+                    phenotype[i].assignedGroup = groupPhenotype[i].code;
+                    groupFuntion(phenotype[i]);
+                    continue;
+                }
+                else if (phenotype[i].normalSpaces <= students && phenotype[i].accessibleSpaces <= specialStudents)
+                {    
+                    score = 5;
+                    phenotype[i].assignedGroup = groupPhenotype[i].code;
+                    groupFuntion(phenotype[i])
+                }
+                else if (phenotype[i].normalSpaces > students && phenotype[i].accessibleSpaces > specialStudents)
+                {    
+                    score = 0;
+                    phenotype[i].assignedGroup = groupPhenotype[i].code;
+                    groupFuntion(phenotype[i]);
+                }
             }
-            else if (phenotype.normalSpaces == students && phenotype.accessibleSpaces <= specialStudents)
-                score = 8;
-            else if (phenotype.normalSpaces <= students && phenotype.accessibleSpaces <= specialStudents)
-                score = 5;
-            else if (phenotype.normalSpaces > students && phenotype.accessibleSpaces > specialStudents)
-                score = 0;
-        }
+        }).catch(err => {throw err});        
         return score
     }
 
     var groupFuntion = function(phenotype) {
-        json = phenotype;
-    }    
-    
+        json.push(phenotype);
+        console.log(JSON.stringify(json));
+    }            
+
     var geneticAlgorithm;
 
     return new Promise (function(resolve, reject) {
