a223354ffa9f10a83121c9f53943f69b1bbba83d
Vitor Oliveira <endrunne@protonmail.ch>
2020-11-21 11:27:08 -0300

    Remoção de service desnecessária

--- API/src/assignGroup/schemaAssignGroup.js
+++
@@ -1,7 +0,0 @@
-const restful  = require('node-restful')
-
-const schemaAssignGroup = new restful.mongoose.Schema({
-    assignedGroup: { type: String},    
-})
-
-module.exports = restful.model('assignGroups', schemaAssignGroup)
--- API/src/assignGroup/serviceAssignGroup.js
+++
@@ -1,6 +0,0 @@
-const schemaAssignGroup = require('./schemaAssignGroup.js')
-
-schemaAssignGroup.methods(['get'])
-schemaAssignGroup.updateOptions({ new: true, runValidators: true})
-
-module.exports = schemaAssignGroup
